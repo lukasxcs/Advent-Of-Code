39a88d52c8cbdd0871f2e99a6e089029f3b9addb
lukasxcs <lukas.shp02@gmail.com>
2025-12-03 11:27:51 +0100

    Day 1 part 2 done

--- 2025/lib/days/day01.rb
+++ 2025/lib/days/day01.rb
@@ -17,6 +17,19 @@ class Day01 < Base
   end
 
   def part2
-    "Hello Day 1 part 2"
+    dial = 50
+    zero_count = 0
+
+    input.each do |line|
+      direction = line[0]
+      amount = line[1..].to_i
+
+      amount.times do
+        dial = direction == "L" ? (dial - 1) % 100 : (dial + 1) % 100
+        zero_count += 1 if dial == 0
+      end
+    end
+
+    zero_count
   end
 end
